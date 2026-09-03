cd9cfd879259912dfe65b8b6a5234bcb0f465293
fabianoic <fabiano.fic@gmail.com>
2026-09-03 20:44:22 +0100

    creating a util class for tests

---
+++ src/test/java/com/ficsolution/roster/util/Util.java
@@ -0,0 +1,14 @@
+package com.ficsolution.roster.util;
+
+import java.util.UUID;
+
+public class Util {
+
+    public static final UUID storeId = UUID.fromString("8ba57eb8-9345-4068-bc65-d63d9575e4bd");
+    public static final UUID roleId = UUID.fromString("9aa2510e-feda-4b1f-bffe-21cc9e180e9f");
+    public static final UUID employeeId = UUID.fromString("ebb6ea55-b853-4504-a381-0f59ad2a1659");
+    public static final UUID shiftId = UUID.fromString("4355e858-65ba-4e06-b5fd-87c085a47aab");
+    public static final UUID availabilityId = UUID.fromString("f38d39e2-df1c-4df7-86b6-5cb10571376a");
+    public static final UUID timeOffRequestId = UUID.fromString("6df1b206-60aa-4183-8f63-3db9f5a04f77");
+    public static final UUID shiftSwapRequestId = UUID.fromString("78c2dfad-487d-4405-920d-ef89eff01040");
+}
